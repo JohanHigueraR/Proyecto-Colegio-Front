b208e5f2c1b27b9d2703cff82b0e1e5f183266ad
JohanHigueraR <higuerad321@gmail.com>
2022-12-10 23:57:38 -0500

    first commit

---
+++ src/Componentes/Asignaturas.js
@@ -0,0 +1,89 @@
+import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  List,
+  ListItemButton,
+  ListItem,
+  ListItemText,
+  Stack
+} from "@mui/material";
+import { useState, useEffect } from "react";
+function ListaAsignaturas() {
+  const navigate = useNavigate();
+  const [asignatura, setAsignatura] = useState([]);
+  const cargarAsignatura = async () => {
+    const response = await fetch("http://localhost:4000/asignaturas");
+    const data = await response.json();
+    setAsignatura(data);
+  };
+  const archivarAsignatura = async (id) => {
+    const body = {
+      id_asignatura: id,
+    };
+    fetch("http://localhost:4000/asignaturas", {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: { "Content-type": "application/json" },
+    });
+  };
+  useEffect(() => {
+    cargarAsignatura();
+  }, []);
+  return (
+    <>
+      <h1>Lista de Asignaturas</h1>
+      <Button
+        variant="contained"
+        onClick={() => navigate("/asignaturas/crearasignatura")}
+      >
+        Crear Asignatura
+      </Button>
+      <List>
+        {asignatura.map((asig) => (
+          <ListItem
+            disablePadding
+            sx={{
+              backgroundColor: "rgba(0, 0, 0, 0.486)",
+              marginBottom: "1px",
+              "&:hover": {
+                backgroundColor: "rgba(0, 0, 0, 4)",
+              },
+            }}
+            key={asig.id_asignatura}
+          >
+            <ListItemButton>
+              <ListItemText>{asig.nombre_asignatura}</ListItemText>
+              <Stack
+                direction="row"
+                justifyContent="flex-end"
+                alignItems="center"
+                spacing={2}
+              >
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() =>
+                    navigate(
+                      "/asignaturas/" + asig.id_asignatura + "/actualizar"
+                    )
+                  }
+                >
+                  Actualizar
+                </Button>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  onClick={() => archivarAsignatura(asig.id_asignatura)}
+                >
+                  Eliminar
+                </Button>
+              </Stack>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default ListaAsignaturas;
---
+++ src/Componentes/Formcurso.js
@@ -0,0 +1,117 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  Card,
+  Grid,
+  Typography,
+  CardContent,
+  TextField,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+
+function Formcurso() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const [curso, setCurso] = useState({
+    nombre_curso: "",
+    director_curso: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [editar, setEditar] = useState(false);
+  const handleChange = (e) => {
+    setCurso({ ...curso, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (editar) {
+        await fetch("http://localhost:4000/cursos/"+params.id+"/editarcurso", {
+        method: "PUT",
+        body: JSON.stringify(curso),
+        headers: { "Content-type": "application/json" },
+      });
+      setLoading(false);
+      navigate("/cursos");
+    } else {
+        await fetch("http://localhost:4000/cursos/crearcurso", {
+        method: "POST",
+        body: JSON.stringify(curso),
+        headers: { "Content-type": "application/json" },
+      });
+    }
+
+    setLoading(false);
+    navigate("/cursos");
+  };
+  const cargarCurso = async (id) => {
+    const response = await fetch(
+      "http://localhost:4000/cursos/" + id + "/detallescurso"
+    );
+    const data = await response.json();
+    setCurso({ nombre_curso: data.nombre_curso, director_curso: data.director_curso });
+    setEditar(true);
+  };
+  useEffect(() => {
+    if (params.id) {
+      cargarCurso(params.id);
+    }
+  }, [params.id]);
+  return (
+    <Grid container alignItems="center" justifyContent="center">
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }}
+          style={{ backgroundColor: "#1e272e", padding: "1rem" }}
+        >
+          <Typography variant="h5" textAlign={"center"} color="white">
+            {editar ? "Editar Curso": "Crear Curso"}
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                variant="filled"
+                label="Nombre del curso:"
+                sx={{ display: "block", margin: "0.5rem" }}
+                inputProps={{ style: { color: "white" } }}
+                InputLabelProps={{ style: { color: "white" } }}
+                name="nombre_curso"
+                value={curso.nombre_curso}
+                onChange={handleChange}
+              ></TextField>
+              <TextField
+                variant="filled"
+                label="Nombre del director:"
+                sx={{ display: "block", margin: "0.5rem" }}
+                inputProps={{ style: { color: "white" } }}
+                InputLabelProps={{ style: { color: "white" } }}
+                name="director_curso"
+                value={curso.director_curso}
+                onChange={handleChange}
+              ></TextField>
+              
+
+              <Button
+                variant="contained"
+                style={{ marginLeft: "3rem" }}
+                type="sumbit"
+                disabled={!curso.nombre_curso || !curso.director_curso}
+              >
+                {loading ? (
+                  <CircularProgress
+                    color="inherit"
+                    size={24}
+                  ></CircularProgress>
+                ) : (
+                  "Guardar"
+                )}
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Formcurso;
