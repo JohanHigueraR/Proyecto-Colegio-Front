0453dd6f2cbe660747b78096e2e116b1df0cb475
Johan Daniel Higuera <93680528+JohanHigueraR@users.noreply.github.com>
2023-02-16 15:36:10 -0500

    comprobación conflictos

--- src/Componentes/Aministracion.js/AdminNotas.js
+++ src/Componentes/Aministracion.js/AdminNotas.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import { Button, Stack } from "@mui/material";
@@ -35,42 +34,4 @@ function AdminNotas() {
       );
 }
 
-=======
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import { Button, Stack } from "@mui/material";
-import Admincursos from "./Admincursos";
-import Adminasignaturas from "./Adminasignaturas";
-import Adminestudiantes from "./Adminestudiantes";
-
-function AdminNotas() {
-    return (
-        <div style={{ margin: "2rem" }}>
-          <Typography textTransform={"uppercase"} variant={"h5"}>
-            Lista de Archivados
-          </Typography>
-          <Stack
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="center"
-            spacing={2}
-          >
-            <Button variant={"contained"} color={"info"} onClick={<Admincursos></Admincursos>}>
-              Cursos
-            </Button>
-            <Button variant={"contained"} color={"info"} onClick={<Adminasignaturas></Adminasignaturas>}>
-              Asignaturas
-            </Button>
-            <Button variant={"contained"} color={"info"} onClick={<Adminestudiantes></Adminestudiantes>}>
-              Estudiantes
-            </Button>
-            <Button variant={"contained"} color={"info"} onClick={<AdminNotas></AdminNotas>}>
-              Notas
-            </Button>
-          </Stack>
-        </div>
-      );
-}
-
->>>>>>> b754d4ab2c15008b3fab54500ca08345d8087dbc
 export default AdminNotas
--- src/Componentes/Materias.js
+++ src/Componentes/Materias.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 function Materias() {
@@ -7,14 +6,4 @@ function Materias() {
   )
 }
 
-=======
-import React from 'react'
-
-function Materias() {
-  return (
-    <div>Materias</div>
-  )
-}
-
->>>>>>> b754d4ab2c15008b3fab54500ca08345d8087dbc
 export default Materias
